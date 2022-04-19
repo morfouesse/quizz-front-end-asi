b49d42eb48823f52c15b661c4d12cea09cf4cd3a
morfouesse <antoine.morganmorfouesse@gmail.com>
2022-04-19 15:09:07 +0200

    test MR x2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CoreModule} from "./core/core.module";
 import {SurveysModule} from "./surveys/surveys.module";
+
 
 @NgModule({
   declarations: [
