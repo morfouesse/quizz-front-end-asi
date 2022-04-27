cb9487b731447e493e894ac58d85088b2c9ffdae
morfouesse <antoine.morganmorfouesse@gmail.com>
2022-04-27 17:32:36 +0200

    form is working

--- src/app/surveys/components/form/survey-form-stepper/survey-form-stepper.component.ts
+++ src/app/surveys/components/form/survey-form-stepper/survey-form-stepper.component.ts
@@ -1,29 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import {
-  AbstractControl,
-  FormArray,
-  FormBuilder,
-  FormGroup,
-  ValidationErrors,
-  ValidatorFn,
-  Validators
-} from "@angular/forms";
-import {SurveyHttpClientService} from "../../../services/survey-http-client.service";
-import {Router} from "@angular/router";
-import {tap} from "rxjs";
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {CustomsValidatorsService} from "../../../../shared/services/customs-validators.service";
 
 @Component({
   selector: 'app-survey-form-stepper',
   templateUrl: './survey-form-stepper.component.html',
-  styleUrls: ['./survey-form-stepper.component.less']
+  styleUrls: ['./survey-form-stepper.component.less'],
 })
 export class SurveyFormStepperComponent implements OnInit {
 
-  surveyForm!: FormGroup;
-//TODO: custom validation à faire
-  private nameRe: RegExp = new RegExp("/bob/i");
+  public surveyForm!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private surveyHttpClientService: SurveyHttpClientService, private router: Router) {
+  constructor(private formBuilder: FormBuilder, private customValidatorsService: CustomsValidatorsService) {
   }
 
   ngOnInit(): void {
@@ -35,67 +23,35 @@ export class SurveyFormStepperComponent implements OnInit {
   createSurveyForm(): FormGroup {
 
     return this.surveyForm = this.formBuilder.group({
-      title: [null, [
+      title: ['', [
         Validators.required,
         Validators.minLength(5),
-        // this.unAuthrorizedNameValidator(this.nameRe)
+        this.customValidatorsService.noWhiteSpaceValidator()
       ]],
-      description: [null, [
+      description: ['', [
         Validators.required,
         Validators.minLength(5),
+        this.customValidatorsService.noWhiteSpaceValidator()
       ]],
       questions: this.formBuilder.array([
         this.formBuilder.group({
-          title: [null, [
+          title: ['', [
             Validators.required,
             Validators.minLength(5),
+            this.customValidatorsService.noWhiteSpaceValidator()
           ]],
           answers: this.formBuilder.array([
             this.formBuilder.group({
-              name: [null, [
+              name: ['', [
                 Validators.required,
-                Validators.minLength(5),
+                Validators.minLength(2),
+                this.customValidatorsService.noWhiteSpaceValidator()
               ]],
-              goodAnswer: [false, [
-              ]],
-              question: [null,[
-
-              ]]
+              goodAnswer: [false, []],
             })
           ])
         })
       ])
     })
   }
-
-  get questions(): FormArray {
-    return this.formSurvey?.get('questions') as FormArray;
-  }
-
-  addQuestion(): void {
-    this.questions.push(this.formBuilder.control(''));
-  }
-
-  get answers(): FormArray {
-    return this.formSurvey?.get('questions')?.get([0])?.get('answers') as FormArray;
-    //return this.formArray?.get([1])?.get('questions') as FormArray;
-  }
-
-  get formSurvey(): AbstractControl | null {
-    return this.surveyForm;
-  }
-
-  unAuthrorizedNameValidator(nameRe: RegExp): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-      const name = nameRe.test(control.value);
-      return name ? {unAuthorizedName: {value: control.value}} : null;
-    }
-  }
-
-  onSubmitForm() {
-    this.surveyHttpClientService.addSurveyWithTheseQuestionsAndAnswers(this.surveyForm).pipe(
-      tap(() => this.router.navigateByUrl('/surveys'))
-    ).subscribe();
-  }
-
 }
